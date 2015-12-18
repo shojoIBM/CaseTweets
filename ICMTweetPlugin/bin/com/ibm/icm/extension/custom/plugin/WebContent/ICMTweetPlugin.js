50262d1538faadd146e019eefbd8e3652d3330f3
julialaiIBM <julia.lai@us.ibm.com>
2015-12-17 17:19:21 -0800

    update build.xml

---
+++ ICMTweetPlugin/bin/com/ibm/icm/extension/custom/plugin/WebContent/ICMTweetPlugin.js
@@ -0,0 +1,13 @@
+var icmContextRoot = "/ICMTweetWidget";
+var paths = {
+	"icmtweet": icmContextRoot + "/icmtweet"
+};
+require({paths:paths});
+
+require(["dojo/_base/declare",
+	"dojo/_base/lang"], 
+function(declare, lang) {
+	/**
+	 * Use this function to add any global JavaScript methods your plug-in requires.
+	 */
+});
